7c34e6898625b5c3d78c12826de6f64c3861633d
Jonathan Hawk <jonathan@libreworks.com>
2015-05-30 13:50:53 -0400

    Adding Map interface

---
+++ src/Caridea/Util/Map.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Caridea
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ * 
+ * @copyright 2015 LibreWorks contributors
+ * @license   http://opensource.org/licenses/Apache-2.0 Apache 2.0 License
+ */
+namespace Caridea\Util;
+
+/**
+ * A Key-value Map.
+ * 
+ * @copyright 2015 LibreWorks contributors
+ * @license   http://opensource.org/licenses/Apache-2.0 Apache 2.0 License
+ */
+interface Map extends \Countable, \IteratorAggregate, \ArrayAccess
+{
+    /**
+     * Removes all values.
+     */    
+    public function clear();
+
+    /**
+     * Gets a single value, or return `$alt` if not found.
+     * 
+     * @param string $key The name of the value
+     * @param mixed $alt Optional default value to return
+     * @return mixed The value, or the `$alt` if not found
+     */
+    public function get($key, $alt = null);
+    
+    /**
+     * Adds all key-value pairs from the supplied `$values` to this one.
+     * 
+     * Values in the incoming `HashTable` with identical keys will overwrite
+     * those in this one.
+     * 
+     * @param \Caridea\Util\Map $values The incoming values
+     */
+    public function merge(Map $values);
+}
---
+++ src/Caridea/Util/NullMap.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Caridea
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ * 
+ * @copyright 2015 LibreWorks contributors
+ * @license   http://opensource.org/licenses/Apache-2.0 Apache 2.0 License
+ */
+namespace Caridea\Util;
+
+/**
+ * A no-op key-value Map.
+ * 
+ * @copyright 2015 LibreWorks contributors
+ * @license   http://opensource.org/licenses/Apache-2.0 Apache 2.0 License
+ */
+class NullMap implements Map
+{
+    public function clear()
+    {
+    }
+
+    public function count()
+    {
+        return 0;
+    }
+
+    public function get($key, $alt = null)
+    {
+        return $alt;
+    }
+
+    public function getIterator()
+    {
+        return new \EmptyIterator();
+    }
+
+    public function merge(Map $values)
+    {
+    }
+    
+    public function offsetExists($offset)
+    {
+        return false;
+    }
+
+    public function offsetGet($offset)
+    {
+        return null;
+    }
+
+    public function offsetSet($offset, $value)
+    {
+    }
+
+    public function offsetUnset($offset)
+    {
+    }
+}
